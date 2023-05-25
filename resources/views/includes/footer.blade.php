0508407c77584bc7db0b521ca8bea3a0a95b835c
Mufti Restu Mahesa <mufti.restumahesa@gmail.com>
2023-05-25 23:19:32 +0700

    update footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
         <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright &copy;
-            SD Negeri 02 Bengkulu - 2023</span>
+            SD Negeri 02 Bengkulu Tengah - 2023</span>
         <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Developed By : Aprieza Putri (G1A020015)</span>
     </div>
 </footer>
